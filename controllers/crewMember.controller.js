6798925a25991fbfc9822999b9ecd3d51ae613d0
JustineM <justine.merlin94@gmail.com>
2022-05-30 11:07:39 +0200

    Add updateMember and GetMember method to controller

--- controllers/crewMember.controller.js
+++ controllers/crewMember.controller.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const CrewMemberModel = require('../models/crewMember.model');
 
+module.exports.getMembers = async (req, res) => {
+    const members = await CrewMemberModel.find();
+    res.status(200).json(members);
+}
+
 module.exports.addMember = async(req, res) => {
     const { name, qualifiers } = req.body;
     try {
@@ -10,4 +15,23 @@ module.exports.addMember = async(req, res) => {
         catch(err) {
             res.status(200).send({err});
         }
+}
+
+module.exports.updateMember = async (req, res) => {
+    // Checking if users exists
+    if (!mongoose.isValidObjectId(req.params.id))
+        res.status(400).send("Unknown Member's ID : " + req.params.id)
+    
+    const member = await CrewMemberModel.findById(req.params.id);
+    member.name = req.body.name;
+    if(req.body.qualifiers) {
+        member.qualifiers = req.body.qualifiers;
+    }
+    
+    try {
+        await member.save();
+        res.status(200).json(member)
+    } catch(err) {
+        res.status(500).json({err})
+    }
 }
--- routes/crewMember.routes.js
+++ routes/crewMember.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const crewMemberController = require('../controllers/crewMember.controller');
 
+router.get('/', crewMemberController.getMembers);
 router.post('/register', crewMemberController.addMember);
+router.put('/:id', crewMemberController.updateMember);
 
 module.exports = router;
