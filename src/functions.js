83e38b782467fca9bfb2433aaa10f625daeaad07
JustineM <justine.merlin94@gmail.com>
2022-06-03 16:54:49 +0200

    add api url as env variable

--- src/functions.js
+++ src/functions.js
@@ -1,11 +1,13 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const getMembers = async () => {
-    const response = await fetch('https://argocrew-api.herokuapp.com/api/member/', {mode: 'cors'});
+    const response = await fetch(`${API_URL}/`, {mode: 'cors'});
     const responseJson = await response.json();
     return responseJson
 }
 
 export const addMember = async (newMember) => {
-    const response = await fetch('https://argocrew-api.herokuapp.com/api/member/register', {
+    const response = await fetch(`${API_URL}/register`, {
       method: 'POST',
       headers: {
         'Content-Type' : 'application/json',
