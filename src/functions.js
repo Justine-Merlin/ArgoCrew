9e804e63f0eb39d2de04cb47087be1be178396dc
Justine-Merlin <85105369+Justine-Merlin@users.noreply.github.com>
2023-05-06 17:46:06 +0200

    Edit fetch function

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,7 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const getMembers = async () => {
-    const response = await fetch(`${API_URL}/`, {mode: 'cors'});
+    const response = await fetch(`${API_URL}`, {mode: 'cors'});
     const responseJson = await response.json();
     return responseJson
 }
