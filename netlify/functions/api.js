30413d007a65784d7831a69cfaa2ac64e11ebe1e
Justine-Merlin <justine.merlin94@gmail.com>
2023-07-07 14:42:11 +0200

    import routes fix

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
+import crewMemberRoutes from "../../routes/crewMember.routes";
+
 const api = express();
 
 const router = Router();
