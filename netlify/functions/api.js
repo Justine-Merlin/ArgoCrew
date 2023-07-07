1513db19e38322bcbec6bdf6d34e3cae3ab72a26
Justine-Merlin <justine.merlin94@gmail.com>
2023-07-07 14:53:56 +0200

    new test for routes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,13 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
-
 import crewMemberRoutes from "../../routes/crewMember.routes";
 
 const api = express();
 
-// const router = Router();
-// api.use("/members", crewMemberRoutes);
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", crewMemberRoutes);
+api.use("/api/", router);
+api.use("/members/", crewMemberRoutes);
 
 export const handler = serverless(api);
