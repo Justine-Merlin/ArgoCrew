b3ef59631d9840d5a2c5f40e79d16a6ca0511c02
Justine-Merlin <justine.merlin94@gmail.com>
2023-07-07 15:04:38 +0200

    routes inside api file

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,17 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
-import crewMemberRoutes from "../../routes/crewMember.routes";
+const crewMemberRoutes = require("../../routes/crewMember.routes");
 
 const api = express();
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/members", crewMemberController.getMembers);
+router.post("/members/register", crewMemberController.addMember);
+router.put("/members/:id", crewMemberController.updateMember);
+router.delete("/members/:id", crewMemberController.deleteMember);
 
 api.use("/api/", router);
-api.use("/members/", crewMemberRoutes);
+// api.use("/members/", crewMemberRoutes);
 
 export const handler = serverless(api);
