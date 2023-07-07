08a4b2aca41be79eafd0a577b2cbe144ff55a120
Justine-Merlin <justine.merlin94@gmail.com>
2023-07-07 14:39:53 +0200

    adapt routes to deployment

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,7 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+api.use("/members", crewMemberRoutes);
 
 api.use("/api/", router);
 
